c59f5221a990ee66f3050ce0f08844d924ac0c46
Rowan Strang <rowanstrang@gmail.com>
2025-05-20 16:12:03 +1200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by MacRow on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
